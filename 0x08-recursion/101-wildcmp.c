614ac26f040e025de44c8580871bca460fba868f
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-04 21:10:43 +0000

    Comparing two strings weda dey are identical, Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -31,9 +31,9 @@ int str_checker(char *s1, char *s2, int i, int j)
 		return (1);
 	if (s1[i] == s2[j])
 		return (str_checker(s1, s2, i + 1, j + 1));
-	if (s1[i] == '\0' && s2[j] == '42')
+	if (s1[i] == '\0' && s2[j] == '*')
 		return (str_checker(s1, s2, i, j + 1));
-	if (s2[j] == '42')
+	if (s2[j] == '*')
 		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
 	return (0);
 
