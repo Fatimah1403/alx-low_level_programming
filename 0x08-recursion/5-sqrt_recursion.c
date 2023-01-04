d09db7fde838c18d308ff862bb709d8c8de4ccc9
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-04 18:06:15 +0000

    Square root , task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -35,12 +35,6 @@ int _sqrt_recursion(int n)
 	if (n == 0)
 		return (0);
 
-	else if (n < 0)
-		return (-1);
-
-	else if (n == 1)
-		return (1);
-
-		return (check_sqrt(n, 1));
+	return (check_sqrt(n, 1));
 
 }
