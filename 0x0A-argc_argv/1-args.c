0d5e666aadb8b9e64f9d51eb8409041b03d8a6b6
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-06 22:56:34 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of a program
+ *
+ * @argc: argument count/ first argument
+ * @argv: argument vector/ second argument
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("argc = %d\n", argc);
+
+	return (0);
+}
