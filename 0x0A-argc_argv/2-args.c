c9ef76609c3fc1baf64ba13652fb95dfc3026604
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-06 23:42:14 +0000

    Task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -14,10 +14,10 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	
+
 	for (i = 0; i < argc; i++)
 	{
-		printf("argv[%d] =%s\n",  argv[i]);
+		printf("argv[%d] = %s\n",  argv[i]);
 	}
 
 	return (0);
