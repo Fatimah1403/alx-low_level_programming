32d66de49c41624c65390d10ff5e4006cc52e23e
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-06 23:35:22 +0000

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints all arguments it receives
+ *
+ * @argc: argument count/ first argument
+ * @argv: argument vector/ second argument
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			printf("argv[%d] = %s\n", i, argv[i]);
+		}
+	}
+	return (0);
+
+
+}
