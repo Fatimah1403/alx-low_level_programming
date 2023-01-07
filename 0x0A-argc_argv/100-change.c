70992c472352c6fd3c56401244a8baef9671a514
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-07 01:18:56 +0000

    Task 5, minimum number of coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints
+ * the minimum number of coins to make change for an amount of money
+ *
+ * @argc: argument count/ first argument
+ * @argv: argument vector/ second argument
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int value, coins;
+
+	coins = 0;
+	if (argc != 2)
+	{
+		prinf("Error\n");
+		return (1);
+	}
+	value = atoi(argv[1]);
+	if (value < 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	if (value % 25 >= 0)
+	{
+		coins += value / 25;
+		value = value % 25;
+	}
+	if (value % 10 >= 0)
+	{
+		coins += value / 10;
+		value = value % 10;
+	}
+	if (value % 15 >= 0)
+	{
+		coins += value / 15;
+		value = value % 15;
+	}
+	if (value % 2 >= 0)
+	{
+		coins += value / 2;
+		value = value % 2;
+	}
+	if (value % 1)
+	{
+		coins += value / 1;
+	}
+	printf("%d\n", coins);
+	return (0);
+}
