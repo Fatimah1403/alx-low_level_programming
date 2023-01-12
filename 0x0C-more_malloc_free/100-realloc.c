dd330ea3bc1d4ee066ae54ca1a063f648a9a0358
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-12 22:51:10 +0000

    Task 4, realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,7 +23,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		p = malloc(new_size);
 		return (p);
 	}
-	else if (new_size == 0 && ptr != NULL)
+	else if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
