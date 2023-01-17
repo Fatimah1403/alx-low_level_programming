8aa56b71812046e979b3fb2cff4ddb670186c592
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-17 01:25:42 +0000

    Task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,18 +1,8 @@
 #ifndef _3_FUNCTION_LIKE_MACRO_H
 #define _3_FUNCTION_LIKE_MACRO_H
 
-#include <stdlib.h>
 
-/**
- * ABS - Functionthat computes absolute value of x.
- *
- * @x: integer to check the absolute value
- *
- * Return: Always (0) for success
- */
-int ABS(int x)
-{
-	return ((x < 0) ? (-x) : (x));
-}
+#define ABS(x) ((x < 0) ? (-x) : (x));
+
 
 #endif
