a9690b5db9de27fa629ada3da3344b722f113727
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-17 01:05:36 +0000

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * @argc: nos of arguments passed
+ * @argv: pointer arrays which point to the arguments
+ *
+ * Return: Always (0)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
