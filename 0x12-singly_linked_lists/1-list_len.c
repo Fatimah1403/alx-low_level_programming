c5691c387d20c28fe4dec6baa64f7e43c4a5824a
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-26 20:56:49 +0000

    Task 1 fixed

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,21 @@
-#include "lists.h"
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
 /**
- * list_len -  a function that returns the num of elements of a list_t list
- *
- * @h: the size of nos to be printed
- * Return: the number of nodes
- *
+ * list_len - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
  */
 size_t list_len(const list_t *h)
 {
-	int i = 0;
+	int count = 0;
 
 	while (h)
 	{
-		i++;
+		count++;
 		h = h->next;
 	}
-	return (i);
+	return (count);
 }
