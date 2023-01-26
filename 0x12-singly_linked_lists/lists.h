ea19459d5a38c9632837a1cc601ba0001a665807
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-26 20:58:48 +0000

    Task 1 fixed

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib>
 
 /**
  * struct list_s - singly linked list
@@ -10,18 +9,15 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for ALX project
+ * for Holberton project
  */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-
 } list_t;
 
-
-int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
