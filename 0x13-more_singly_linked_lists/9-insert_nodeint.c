0eea79d5cae286fc5c2675c6e1e7e89dbdc13eca
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-30 22:09:24 +0000

    Task 9, insertion of a new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,32 +16,39 @@
   */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *copy = *head;
-	unsigned int node;
+	listint_t *new_node, *iter;
+	unsigned int count = 1;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-
-	if (idx == 0)
+	if (head)
 	{
-		new->next = copy;
-		*head = new;
-		return (new);
-	}
-
-	for (node = 0; node < (idx - 1); node++)
-	{
-		if (copy == NULL || copy->next == NULL)
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
 			return (NULL);
 
-		copy = copy->next;
+		new_node->n = n;
+		if (idx > 0)
+		{
+			iter = *head;
+			while (iter)
+			{
+				if (count == idx)
+				{
+					new_node->next = iter->next;
+					iter->next = new_node;
+					return (new_node);
+				}
+				iter = iter->next;
+				count++;
+			}
+			if (idx > count)
+				return (NULL);
+		}
+		else
+		{
+			new_node->next = *head;
+			*head = new_node;
+		}
+		return (new_node);
 	}
-
-	new->next = copy->next;
-	copy->next = new;
-
-	return (new);
+	return (NULL);
 }
