76af4640a7f93f29a8f29ec1791bb9458a09672f
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-30 16:09:15 +0000

    Task 1, returninh nos of element

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -12,10 +12,13 @@ size_t listint_len(const listint_t *h)
 {
 	int count = 0;
 
-	while (h)
+	if (h != NULL)
 	{
-		count++;
-		h = h->next;
+		while (h)
+		{
+			count++;
+			h = h->next;
+		}
 	}
 	return (count);
 
