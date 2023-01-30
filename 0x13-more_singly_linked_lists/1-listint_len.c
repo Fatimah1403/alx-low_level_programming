24159e97b96197dd20096e5f2402268fcaba463d
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-30 16:04:19 +0000

    Task 1, returninh nos of element

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - a function that returns the number of elements in a linked
+ * @h: name of the list
+ * Return: the number of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+
+}
