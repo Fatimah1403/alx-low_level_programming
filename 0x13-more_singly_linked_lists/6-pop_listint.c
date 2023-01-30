2f8ec6450e1accb4286c1041b5388a8258ba29aa
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-30 18:35:16 +0000

    Task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,7 +9,7 @@
   * listint_t linked list, and returns the head node’s data (n)
   * @head: The head of the linked list
   *
-  * Return: Nothing
+  * Return: the haed node
   */
 int pop_listint(listint_t **head)
 {
