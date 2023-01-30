52b9e2a8a74d1f8c7ab7c0d4b571c4afcb30697f
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-30 22:02:45 +0000

    Task 9, insertion of a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,8 +16,8 @@
   */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *current;
 	unsigned int i;
+	listint_t *current, *new;
 
 	if (head == NULL)
 		return (NULL);
@@ -31,21 +31,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (current == NULL)
 			return (NULL);
 	}
-
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
-
-	new_node->n = n;
+	new->n = n;
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
-	new_node->next = current->next;
-	current->next = new_node;
-
-	return (new_node);
-
+	new->next = current->next;
+	current->next = new;
+	return (new);
 }
