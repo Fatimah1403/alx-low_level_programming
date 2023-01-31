3a6b35fe805067a3236280c68c2283e73269c1b9
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-01-31 10:34:43 -0500

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@
   */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *current, *next, *temp;
+	listint_t *current, *next;
 	unsigned int i;
 
 	if (head == NULL || *head == NULL)
