53afc0d95de57cadc8c46d82d4227e134f93ac68
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-31 17:44:46 +0000

    task 12, print(safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,8 +1,6 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * _r - reallocates memory for an array of pointers
@@ -18,7 +16,7 @@ const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 	const listint_t **newlist;
 	size_t i;
 
-	newlist = malloc(sizeof(listint_t) * size);
+	newlist = malloc(size * sizeof(listint_t *));
 	if (newlist == NULL)
 	{
 		free(list);
@@ -29,36 +27,35 @@ const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 	newlist[i] = new;
 	free(list);
 	return (newlist);
-
 }
 
 /**
- * print_listint_safe - a function that reverses a listint_t linked list
- * @head: name of the list
- * Return: the number of nodes.
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer to the start of the list
+ *
+ * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i, count = 0;
-	const listint_t **list = Null;
+	size_t i, num = 0;
+	const listint_t **list = NULL;
 
 	while (head != NULL)
 	{
-		for (i = 0; i < count; i++)
+		for (i = 0; i < num; i++)
 		{
 			if (head == list[i])
 			{
-				pintf("-> [%p] %d\n", (void *)head, head->n);
+				printf("-> [%p] %d\n", (void *)head, head->n);
 				free(list);
-				return (count);
+				return (num);
 			}
-			count++;
-			list = _r(list, count, head);
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
 		}
-		free(list);
-		return (count);
+		num++;
+		list = _r(list, num, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
-
+	free(list);
+	return (num);
 }
