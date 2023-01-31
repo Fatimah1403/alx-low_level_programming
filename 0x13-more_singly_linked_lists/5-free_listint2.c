d0af18e9f6b4824bcbfaa55ee0ff18214555adc2
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-31 20:18:33 +0000

    Task 5 update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,17 +9,17 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
 
+	temp = *head;
 	while (*head)
 	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
+		temp = *head;
+		(*head) = (*head)->next;
+		free(temp);
 	}
-
-	head = NULL;
+	*head = NULL;
 }
