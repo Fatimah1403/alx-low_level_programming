fe6477293e37c33700ce4bb823f3cfbd98608ea4
Fatimah1403 <fattyhassan14@gmail.com>
2023-01-31 19:39:20 +0000

    Task 14,finding loop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * find_listint_loop - a function that finds the loop in a linked list.
+ * @head: name of the list
+ * Return: the number of nodes.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow, *fast;
+
+	slow = fast = head;
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			slow = head;
+			break;
+		}
+	}
+	if (!slow || !fast || !fast->next)
+		return (NULl);
+	while (slow != fast)
+	{
+		slow = slow->next;
+		fast = fast->next;
+	}
+	return (fast);
+}
