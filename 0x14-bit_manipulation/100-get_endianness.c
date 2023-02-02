674f961a24efe0917caceb617da665ec74ed3efc
Fatimah1403 <fattyhassan14@gmail.com>
2023-02-02 23:42:45 +0000

    Task 6, endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * get_endianness - a function that checks the endianness.
+  *
+  * Return:  0 if big endian, 1 if little endian
+  */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *)&i;
+
+	if (*c)
+		printf("Little enndian\n");
+	else
+		printf("Big endian\n");
+	return (0);
+}
