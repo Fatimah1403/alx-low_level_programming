47ebb85579b53254bc045ee8ce34586049f78529
Fatimah1403 <fattyhassan14@gmail.com>
2023-02-02 19:09:10 +0000

    Task 0 hot fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,16 +10,39 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int val = 0;
-	int i = 0;
+	unsigned int len = 0, count = 0, sum = 0;
+	int i;
 
 	if (b == NULL)
 		return (0);
-	while (b[i] == '0' || b[c] == '1')
+	len = strlen(b);
+
+	for (i = len - 1; i >= 0; i--)
 	{
-		val <<= 1;
-		val += b[i] - '0';
-		i++;
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		if (b[i] == '1')
+		{
+			sum += 1 << count;
+			count++;
+		}
+		return (sum);
 	}
-	return (val);
+
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
 }
