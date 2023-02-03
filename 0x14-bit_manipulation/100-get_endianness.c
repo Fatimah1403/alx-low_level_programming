aebea9cee3e8b0ff0e638b5edb59e72caec03da2
Fatimah1403 <fattyhassan14@gmail.com>
2023-02-03 00:37:53 +0000

    Task 6 endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -13,9 +13,8 @@ int get_endianness(void)
 	unsigned int i = 1;
 	char *c = (char *)&i;
 
-	if (*c)
-		printf("Little enndian\n");
-	else
-		printf("Big endian\n");
+	if (*c == 1)
+		return (1);
+
 	return (0);
 }
