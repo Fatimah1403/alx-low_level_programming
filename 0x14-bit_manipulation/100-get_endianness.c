6d8eb3b2ca1a0024149fff6c3f27774f2b81cfbe
Fatimah1403 <fattyhassan14@gmail.com>
2023-02-03 00:12:11 +0000

    Task 6, endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -6,7 +6,7 @@
 /**
   * get_endianness - a function that checks the endianness.
   *
-  * Return:  0 if big endian, 1 if little endian
+  * Return:  (0) if big endian, (1) if little endian
   */
 int get_endianness(void)
 {
