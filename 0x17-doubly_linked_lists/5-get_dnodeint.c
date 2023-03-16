2b7c94586794f10548902eac4f1cd86e3a5d34f2
root <fattyhassan14@gmail.com>
2023-03-16 23:06:34 +0000

    Task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		{
 			if (iterate == index)
 				return (current);
-			current current->next;
+			current = current->next;
 			iterate++;
 		}
 	}
