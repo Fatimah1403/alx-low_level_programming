84113e0468f92571add55dfb673dd91a92063de4
root <fattyhassan14@gmail.com>
2023-03-18 17:45:05 +0000

    Task 11 crackme5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -30,6 +30,7 @@ int f4(char *usrn, int len)
 
 	return (rand_num & 63);
 }
+
 /**
  * f5 - multiplies each char of username
  *
@@ -52,6 +53,7 @@ int f5(char *usrn, int len)
 
 	return (((unsigned int)ch ^ 239) & 63);
 }
+
 /**
  * f6 - generates a random char
  *
@@ -70,8 +72,10 @@ int f6(char *usrn)
 		ch = rand();
 		vch += 1;
 	}
+
 	return (((unsigned int)ch ^ 229) & 63);
 }
+
 /**
  * main - Entry point
  *
@@ -83,17 +87,17 @@ int main(int argc, char **argv)
 {
 	char keygen[7];
 	int len, ch, vch;
-	long aiph[] = {
+	long alph[] = {
 		0x3877445248432d41, 0x42394530534e6c37, 0x4d6e706762695432,
 		0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
 		0x723161513346655a, 0x6b756f494b646850 };
 	(void) argc;
 
-	for (len = 0; argc[1][len]; len++)
+	for (len = 0; argv[1][len]; len++)
 		;
-	/* ----------f1 ----------------*/
+	/* ----------- f1 ----------- */
 	keygen[0] = ((char *)alph)[(len ^ 59) & 63];
-	/* -------------f2 -------------*/
+	/* ----------- f2 ----------- */
 	ch = vch = 0;
 	while (vch < len)
 	{
@@ -101,24 +105,23 @@ int main(int argc, char **argv)
 		vch = vch + 1;
 	}
 	keygen[1] = ((char *)alph)[(ch ^ 79) & 63];
-	/* ---------------f3 --------------------*/
+	/* ----------- f3 ----------- */
 	ch = 1;
 	vch = 0;
-
 	while (vch < len)
 	{
 		ch = argv[1][vch] * ch;
 		vch = vch + 1;
 	}
 	keygen[2] = ((char *)alph)[(ch ^ 85) & 63];
-	 /* ----------f4 ----------------*/
+	/* ----------- f4 ----------- */
 	keygen[3] = ((char *)alph)[f4(argv[1], len)];
-	 /* ----------f5 ----------------*/
+	/* ----------- f5 ----------- */
 	keygen[4] = ((char *)alph)[f5(argv[1], len)];
-	 /* ----------f6 ----------------*/
+	/* ----------- f6 ----------- */
 	keygen[5] = ((char *)alph)[f6(argv[1])];
 	keygen[6] = '\0';
 	for (ch = 0; keygen[ch]; ch++)
-		print("%c", keygen[ch]);
+		printf("%c", keygen[ch]);
 	return (0);
 }
