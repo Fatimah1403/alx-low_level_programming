4581f3296aa3d4146da82648257a37c111095603
Fatimah 1403 <fattyhassan14@gmail.com>
2023-04-08 02:28:37 +0000

    Task 0 update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,25 +10,25 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table_created;
+	hash_table_t *table;
 	unsigned long int i;
 
 	/* allocate space for the hash table */
-	hash_table_created = malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t));
 
-	if (hash_table_created == NULL)
+	if (table == NULL)
 		return (NULL);
 
-	hash_table_created->size = size;
-	hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
 
-	if (hash_table_created->array == NULL)
+	if (table->array == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
-		hash_table_created->array[i] = NULL;
+		table->array[i] = NULL;
 	}
 
-	return (hash_table_created);
+	return (table);
 }
