cc5af8f7564213020b2fa12a9f3ee6f48b6cca54
Fatimah 1403 <fattyhassan14@gmail.com>
2023-04-08 22:08:54 +0000

    Task 3 update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,44 +10,44 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
-	char *value_copy;
-	unsigned long int index, i;
+	hash_node_t *node;
+	unsigned long int i, index;
+	char value_copy;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	if (ht == NULL || value == NULL || key == NULL || *key == '\0')
 		return (0);
 
 	value_copy = strdup(value);
 	if (value_copy == NULL)
 		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
+	for (i = index; ht->array[i], i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			free(ht->array[i]->value);
+			free(ht->array[i]->value, value);
 			ht->array[i]->value = value_copy;
 			return (1);
 		}
 	}
 
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
 	{
 		free(value_copy);
 		return (0);
+
 	}
-	new->key = strdup(key);
-	if (new->key == NULL)
+	node->key = strdup(key);
+	if (node->key == NULL)
 	{
-		free(new);
+		free(node);
 		return (0);
+
 	}
-	new->value = value_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
+	node->value = value_copy;
+	node->next = ht->array[index];
+	ht->array[index] = node;
 
 	return (1);
-
 }
