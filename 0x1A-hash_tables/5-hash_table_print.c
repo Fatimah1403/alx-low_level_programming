3f2df778bd23ccabe711bbddb4a1c5a07dc05e3a
Fatimah 1403 <fattyhassan14@gmail.com>
2023-04-08 23:01:46 +0000

    Task 5 update

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -19,20 +19,14 @@ void hash_table_print(const hash_table_t *ht)
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
+		node = ht->array[i];
+		while (node != NULL)
 		{
 			if (comma_flag == 1)
 				printf(", ");
-
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				printf("'%s': '%s'", node->key, node->value);
-				node = node->next;
-				if (node != NULL)
-					printf(", ");
-			}
+			printf("'%s', '%s'", node->key, node->value);
 			comma_flag = 1;
+			node = node->next;
 		}
 	}
 	printf("}\n");
