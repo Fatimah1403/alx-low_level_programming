f13332361c1623e1f7b20c7d16104524848cd5b1
Fatimah1403 <fattyhassan14@gmail.com>
2023-07-12 10:35:19 +0000

    Task 7, jumb search alg

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,61 @@
+#include "search_algos.h"
+
+size_t min(size_t a, size_t b);
+
+/**
+ * min - returns the minimum of two size_t values
+ * @a: first value
+ * @b: second value
+ *
+ * Return: `a` if lower or equal to `b`, `b` otherwise
+ */
+size_t min(size_t a, size_t b)
+{
+	if (b > a)
+		return (a);
+	else
+		return (b);
+
+}
+
+/**
+ * jump_search -  function that searches for a value in a
+ * sorted array of integers using the Jump search algorithm
+ *
+ * @array: Array is a pointer to the first element of the array to search in
+ * @size: Number of elements in array
+ * @value: Value to search for in the array
+ *
+ * Return: The first index where value is located
+ * or -1 if value is not found or if array is NULL
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t low;
+	size_t high;
+	size_t step;
+
+	if (!array || size == 0)
+		return (-1);
+
+	step = sqrt(size);
+
+	for (high = 0; high < size && array[high] < value)
+	{
+		low = high;
+		high += step;
+		printf("value checked array[%lu] = [%d]\n",
+			high, array[high]);
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
+	for (; low <= min(high, size - 1); low++)
+	{
+		printf("Value checked array[%lu] = [%d\n]", low, array[low]);
+		if (array[low] == value)
+			return (low);
+	}
+	return (-1);
+
+
+
+}
