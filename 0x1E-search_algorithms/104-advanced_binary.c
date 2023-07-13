4016f8b4b827e6cbb107a1b6d803d5b577f99839
Fatimah1403 <fattyhassan14@gmail.com>
2023-07-13 14:18:44 +0000

    Task 11

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,66 @@
+#include "search_algos.h"
+
+int binary_search_recursion(int *array, int value,
+			    size_t low, size_t high);
+
+/**
+ * binary_search_recursion - helper to `advanced_binary`, recursively searches
+ * for a value in an integer array
+ * @array: pointer to first element of array to seach
+ * @value: value to search for
+ * @low: starting index in array
+ * @high: ending index in array
+ *
+ * Return: index containing `value`, or -1 if `value` not found or
+ * `array` is NULL
+ */
+int binary_search_recursion(int *array, int value,
+			    size_t low, size_t high)
+{
+	size_t mid, i;
+
+	if (!array)
+		return (-1);
+
+	mid = (low + high) / 2;
+	printf("Searching in array: ");
+	for (i = low; i <= high; i++)
+		printf("%i%s", array[i], i == high ? "\n" : ", ");
+
+	if (array[low] == value)
+		return ((int)low);
+
+	if (array[low] != array[high])
+	{
+		if (array[mid] < value)
+			return (binary_search_recursion(array, value,
+							mid + 1, high));
+		if (array[mid] >= value)
+			return (binary_search_recursion(array, value,
+							low, mid));
+	}
+
+	return (-1);
+}
+/**
+ * advanced_binary -  function that searches for a value in a
+ * sorted array of integers using the Jump search algorithm
+ *
+ * @array: Array is a pointer to the first element of the array to search in
+ * @size: Number of elements in array
+ * @value: Value to search for in the array
+ *
+ * Return: The first index where value is located
+ * or -1 if value is not found or if array is NULL
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t low = 0;
+	size_t high = size - 1;
+
+	if (!array)
+		return (-1);
+
+	return (binary_search_recursion(array, value, low, high));
+
+}
